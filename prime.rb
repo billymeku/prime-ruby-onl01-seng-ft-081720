d741e24fab641b74a341b6ab172df8c0c888834a
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-24 00:20:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  new_num =(2..number-1).to_a
+
+ # number.each do|new_num,num|
+ i = 0 
+ num = []
+ while(new_num)
+    if (num % new_num == 0)
+      false 
+    else 
+      true  
+    end
+    i += 1
+  end 
+end
